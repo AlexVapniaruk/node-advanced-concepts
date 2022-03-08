505bfd366a4495b9212ff49c52ba7f72f1142955
Alex V <alexvap23@gmail.com>
2022-03-08 23:12:53 +0200

    remove keys

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,6 @@
 module.exports = {
-  googleClientID: '773147476318-59enepog2piu29n1epss17orct2k5064.apps.googleusercontent.com',
-  googleClientSecret: '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',
-  mongoURI: 'mongodb://alexvap23:jJ7eWpWsAnCEGXN2@emaily-shard-00-00.rwl6y.mongodb.net:27017,emaily-shard-00-01.rwl6y.mongodb.net:27017,emaily-shard-00-02.rwl6y.mongodb.net:27017/emailyDB?ssl=true&replicaSet=atlas-o03hyb-shard-0&authSource=admin&retryWrites=true&w=majority',
-  cookieKey: 'g90gugi3uhg3guhyuighyu3hyugh3yu5g3',
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
 };
